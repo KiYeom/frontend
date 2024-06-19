428a559c23c07eeb76155b44ea6e1c116d544677
ceunnseo <eunseo4068@gmail.com>
2024-06-19 09:35:20 +0900

    feat : 스플래쉬 화면 1. 어플 로딩될 때 쿠키 아이콘이 화면에 보인다. Issue : #4

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Login from "./src/screen/Login"; // 슬래시 확인
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <Text style={styles.text}>Hello, World!</Text>
+      <Login />
     </View>
   );
 };
@@ -22,3 +25,4 @@ const styles = StyleSheet.create({
     color: "#333",
   },
 });
+export default App;
