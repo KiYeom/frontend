d858820bd9d3b8544e474fa0e64ae2e9a1509ff9
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2024-06-29 01:34:37 +0900

    Revert "feat : 구글 로그인"

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,28 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import StartButton from "../components/StartButton";
 import Chat from "./Chat";
 import Setting from "./Setting";
 
 const Home: React.FC = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.txt1}>안녕?," (pretendard 적용)</Text>
-        <Text style={styles.txt2}>안녕?," (적용x)</Text>
-      </View>
-      <View style={styles.center}>
-        <Image
-          source={require("../../assets/cookieSplash.png")}
-          style={styles.image}
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.footer}>
-        <StartButton />
-      </View>
+      <Text style={styles.text}>홈 화면</Text>
     </View>
   );
 };
@@ -30,33 +16,13 @@ const Home: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "yellow",
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {
-    alignItems: "center",
-    marginTop: 50, // 상단에 약간의 여백 추가
-  },
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  footer: {
-    alignItems: "center",
-    marginBottom: 30, // 하단에 약간의 여백 추가
-  },
-  txt1: {
+  text: {
     fontSize: 24,
-    fontFamily: "Pretendard-SemiBold",
-  },
-  txt2: {
-    fontSize: 24,
-  },
-  image: {
-    width: 200,
-    height: 200,
+    color: "#333",
   },
 });
 export default Home;
