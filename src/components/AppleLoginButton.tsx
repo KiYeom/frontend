1aa01550f4290e873a440690f1327d353ae192f2
eunseo <eunseo@eunseoui-MacBookPro.local>
2024-07-15 10:57:07 +0900

    feat : 애플 로그인 애플 버튼 로직 구현 Issue : #52

--- src/components/AppleLoginButton.tsx
+++ src/components/AppleLoginButton.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import * as AppleAuthentication from 'expo-apple-authentication';
 import { View, Text, StyleSheet } from "react-native";
+import { USER, ACCESSTOKEN, REFRESHTOKEN, APP_VERSION } from "../constants/Constants";
+import axios from "axios";
+import { storage } from "../../utils/storageUtils";
+import useIsSignInState from "../store/signInStatus";
+import * as Device from "expo-device";
 
-const AppleLoginButton: React.FC = () => {
+const AppleLoginButton: React.FC<any> = ({navigation}) => {
+  const {isSignIn, setIsSignIn} = useIsSignInState();
   return (
     <View style={styles.container}>
-    <AppleAuthentication.AppleAuthenticationButton
+    <AppleAuthentication.AppleAuthenticationButton //애플 로그인 버튼 생긴거 설정
       buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
       buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
       cornerRadius={5}
@@ -13,14 +19,51 @@ const AppleLoginButton: React.FC = () => {
       onPress={async () => {
         console.log("애플 로그인 버튼 눌림");
         try {
-          const credential = await AppleAuthentication.signInAsync({
+          const credential = await AppleAuthentication.signInAsync({ //로그인하여 유저 정보를 crendential에 저장
             requestedScopes: [
               AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
               AppleAuthentication.AppleAuthenticationScope.EMAIL,
-            ],
-          });
+            ], //애플이 앱으로 접근할 때 요청되는 사용자 정보 범위의 배열 (보안상 처음 로그인 할 때만 유저 정보를 준다)
+          }); 
+          console.log(credential);
           // signed in
+          USER.EMAIL = credential.email ? credential.email : "appleTesting@gmail.com";
+          USER.PROVIDERCODE = credential.user; //고유 id  
+          USER.DEVICEOS = Device.osName;
+          USER.APPVERSION = APP_VERSION;
+
+          axios
+            .post("https://api.remind4u.co.kr/v1/auth/login", {
+              providerName : "apple",
+              providerCode : USER.PROVIDERCODE,
+              deviceId : USER.DEVICEID,
+              appVersion : USER.APPVERSION,
+              deviceOs : USER.DEVICEOS,
+            })
+            .then(function (response) {
+              storage.set(ACCESSTOKEN, response.data.data.accessToken); //access token을 storage에 저장
+              storage.set(REFRESHTOKEN, response.data.data.refreshToken); //refresh token을 storage에 저장
+              USER.NICKNAME = response.data.data.nickname;
+              USER.GENDER = response.data.data.gender;
+              USER.BIRTHDATE = response.data.data.birthdate;
+              setIsSignIn(true); //로그인 성공하면 tabbar 페이지로 이동
+              //navigation.navigate("Main"); //저장하고 메인 페이지로 이동
+              console.log("애플 로그인을 위해 전달한 데이터 : ", response);
+            })
+            .catch(function (error) {
+              setIsSignIn(false);
+              console.log("서버에 애플 로그인 실패");
+              //console.log(error);
+              //console.log(USER.PROVIDERCODE, USER.DEVICEID, USER.APPVERSION, USER.DEVICEOS);
+              //console.log(typeof(USER.PROVIDERCODE), typeof(USER.DEVICEID), typeof(USER.APPVERSION), typeof(USER.DEVICEOS));
+              if (error.response.status == 404) {
+                console.log("애플 유저. 새로운 사용자입니다");
+                navigation.navigate("InfoScreen");
+              }
+            });
         } catch (e) {
+          console.log("살패");
+          console.log(e);
           if (e.code === 'ERR_REQUEST_CANCELED') {
             // handle that the user canceled the sign-in flow
           } else {
