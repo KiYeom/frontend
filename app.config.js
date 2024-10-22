79e419fbb28e35f7b6ad10985ffb82ccf99b7aa6
Yoojung Kim <thjbjs@outlook.com>
2024-10-23 04:32:40 +0900

    feat: 1.2.0 배포

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'reMIND',
     slug: 'reMIND',
-    version: '1.1.5',
+    version: '1.2.0',
     orientation: 'portrait',
     icon: './src/assets/images/appicon.png',
     userInterfaceStyle: 'light',
