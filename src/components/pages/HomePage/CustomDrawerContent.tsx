52b4afe83ca573c0232ed8a79be126d2caab2f70
ceunnseo <eunseo4068@gmail.com>
2025-02-28 14:30:58 +0900

    feat : 이메일 연결

--- src/components/pages/HomePage/CustomDrawerContent.tsx
+++ src/components/pages/HomePage/CustomDrawerContent.tsx
@@ -9,6 +9,28 @@ import {
 import MenuRow from '../../menu-row/menu-row';
 import { Linking, Platform } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
+import * as MailComposer from 'expo-mail-composer';
+import { getUserNickname } from '../../../utils/storageUtils';
+
+const sendMail = async () => {
+  const options = {
+    recipients: ['gwiyeomdungi37@gmail.com'],
+    subject: `🐶쿠키 팬아트 제출🐶`,
+    body: `안녕하세요, reMIND팀입니다. 쿠키에게 잊지 못 할 소중한 기억을 선물해주셔서 감사합니다 :) 쿠키의 팬아트와 함께 아래에 쿠키에게 하고 싶은 말을 남겨주세요. 팬아트 게시판 및 인스타그램에 24시간 내에 업로드가 됩니다!🐶🐾
+    
+    ✨닉네임 (수정 및 삭제 가능)✨ ${getUserNickname()}님
+    💬전달 메세지💬 
+    
+    앞으로도 보호자님께서 쿠키와 행복한 시간을 보내실 수 있도록, 더욱 최선을 다 하는 reMIND팀이 되겠습니다.💚
+    `.replace(/^ +/gm, ''),
+  };
+  const result = await MailComposer.composeAsync(options);
+  if (result.status === 'sent') {
+    console.log('Email sent');
+  } else {
+    console.log('Email not sent');
+  }
+};
 
 const CustomDrawerContent = (props) => {
   //대화체를 관리하는 isCasualMode state
@@ -51,12 +73,7 @@ const CustomDrawerContent = (props) => {
             await Linking.openURL('https://asked.kr/remind_cookie');
           }}
         />
-        <MenuRow
-          text="쿠키 팬아트 보내기"
-          onPress={async () => {
-            await Linking.openURL('https://asked.kr/remind_cookie');
-          }}
-        />
+        <MenuRow text="쿠키 팬아트 보내기" onPress={sendMail} />
       </UserSettingContainer>
     </DrawerContentScrollView>
   );
