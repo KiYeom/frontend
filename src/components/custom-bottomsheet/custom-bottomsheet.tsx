ce78f343e82b8899cfc75fe2937c1d0a1371b2ce
ceunnseo <eunseo4068@gmail.com>
2025-03-21 13:10:46 +0900

    selected 속성 추가

--- src/components/custom-bottomsheet/custom-bottomsheet.tsx
+++ src/components/custom-bottomsheet/custom-bottomsheet.tsx
@@ -7,6 +7,15 @@ import { rsWidth } from '../../utils/responsive-size';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { EmotionIcon } from '../../components/emotionIcon/emotionIcon';
 import Button from '../../components/button/button';
+import {
+  IntroText,
+  IntroContainer,
+  EmotionIconContainer,
+  BottomSheetTextInputContainer,
+  StyledBottomSheetTextInput,
+  ButtonContainer,
+  TextLengthAlert,
+} from './custom-bottomsheet.styles';
 
 interface BottomSheetProps {
   indexNumber?: number;
@@ -30,7 +39,10 @@ const CustomBottomSheet: React.FC<BottomSheetProps> = (props) => {
   const [keyboardHeight, setKeyboardHeight] = useState<number>(0);
   const [text, setText] = useState<string>('');
   const bottomSheetRef = useRef<BottomSheet>(null);
-  const handleClosePress = () => bottomSheetRef.current.close();
+  const handleClosePress = () => {
+    Keyboard.dismiss();
+    bottomSheetRef.current.close();
+  };
   const insets = useSafeAreaInsets();
 
   // 키보드 등장 시 높이 저장
@@ -53,7 +65,7 @@ const CustomBottomSheet: React.FC<BottomSheetProps> = (props) => {
     };
   }, [handleKeyboardDidShow, handleKeyboardDidHide]);
 
-  const snapPoints = ['25%'];
+  const snapPoints = ['35%'];
 
   useEffect(() => {
     if (!bottomSheetRef.current) return;
@@ -94,61 +106,67 @@ const CustomBottomSheet: React.FC<BottomSheetProps> = (props) => {
         style={css`
           flex: 1;
           align-items: center;
-          background-color: red;
+          //background-color: red;
           padding-bottom: ${insets.bottom}px;
           padding-horizontal: ${rsWidth * 20}px;
         `}>
-        <View style={{ backgroundColor: 'yellow' }}>
-          <Text>나의 감정을 자유롭게 표현해보세요</Text>
-        </View>
+        <IntroContainer>
+          <IntroText>나의 감정을 자유롭게 표현해보세요</IntroText>
+        </IntroContainer>
 
-        <View
-          style={css`
-            background-color: green;
-            flex-direction: row;
-            justify-content: space-around;
-            width: 100%;
-          `}>
-          <EmotionIcon status="happy" size={50} onPress={() => handleEmotionPress('happy')} />
-          <EmotionIcon status="angry" size={50} onPress={() => handleEmotionPress('angry')} />
-          <EmotionIcon status="sad" size={50} onPress={() => handleEmotionPress('sad')} />
-          <EmotionIcon status="calm" size={50} onPress={() => handleEmotionPress('calm')} />
-          <EmotionIcon status="normal" size={50} onPress={() => handleEmotionPress('normal')} />
-        </View>
+        <EmotionIconContainer>
+          <EmotionIcon
+            status="happy"
+            size={50}
+            onPress={() => handleEmotionPress('happy')}
+            selected={true}
+          />
+          <EmotionIcon
+            status="angry"
+            size={50}
+            onPress={() => handleEmotionPress('angry')}
+            selected={false}
+          />
+          <EmotionIcon
+            status="sad"
+            size={50}
+            onPress={() => handleEmotionPress('sad')}
+            selected={false}
+          />
+          <EmotionIcon
+            status="calm"
+            size={50}
+            onPress={() => handleEmotionPress('calm')}
+            selected={false}
+          />
+          <EmotionIcon
+            status="normal"
+            size={50}
+            onPress={() => handleEmotionPress('normal')}
+            selected={false}
+          />
+        </EmotionIconContainer>
 
-        <View
-          style={css`
-            background-color: pink;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-          `}>
-          <BottomSheetTextInput
+        <BottomSheetTextInputContainer>
+          <StyledBottomSheetTextInput
             autoFocus
-            placeholder="Click here…"
+            placeholder="지금 느끼는 감정은?"
             value={text}
             onChangeText={setText}
-            style={css`
-              width: 100%;
-              align-items: center;
-              background-color: blue;
-              margin-top: 16px;
-            `}
           />
-          <Text>text : {text}</Text>
-        </View>
-        <Button
-          title="나의 감정 기록하기"
-          primary={true}
-          disabled={validateButton(text)}
-          onPress={() => {
-            console.log('나의 감정 기록하기');
-            handleClosePress();
-            //키보드 내리기
-            Keyboard.dismiss();
-          }}
-        />
+          <TextLengthAlert>{text.length}/10</TextLengthAlert>
+        </BottomSheetTextInputContainer>
+        <ButtonContainer>
+          <Button
+            title="나의 감정 기록하기"
+            primary={true}
+            disabled={validateButton(text)}
+            onPress={() => {
+              console.log('나의 감정 기록하기');
+              handleClosePress();
+            }}
+          />
+        </ButtonContainer>
       </BottomSheetView>
     </BottomSheet>
   );
--- src/components/emotionIcon/emotionIcon.tsx
+++ src/components/emotionIcon/emotionIcon.tsx
@@ -1,19 +1,24 @@
 import { EmotionIconContainer } from './emotion.styles';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import { css } from '@emotion/native';
 import { TIconName } from '../icons/icons';
 import Icon from '../icons/icons';
 type EmotionProps = {
   status?: string;
   size?: number;
   onPress?: () => void;
+  selected?: boolean;
 };
 
 export const EmotionIcon = (props: EmotionProps) => {
-  const { status = 'nomal', size = 30, onPress = () => {} } = props;
+  const { status = 'nomal', size = 30, onPress = () => {}, selected = false } = props;
+  console.log('selected', selected);
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Icon name={`${status}-emotion` as TIconName} width={size + 'px'} height={size + 'px'} />
-    </TouchableOpacity>
+    <View style={{ opacity: selected ? 1 : 0.5 }}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon name={`${status}-emotion` as TIconName} width={size + 'px'} height={size + 'px'} />
+      </TouchableOpacity>
+    </View>
   );
 };
