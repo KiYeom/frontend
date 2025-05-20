5c06b02bb0c71c392a0afc98d8121d089fc89df0
ceunnseo <eunseo4068@gmail.com>
2025-05-20 19:03:17 +0900

    app version 1.7.10 update

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ module.exports = {
   expo: {
     name: 'reMIND',
     slug: 'reMIND',
-    version: '1.7.9',
+    version: '1.7.10',
     orientation: 'portrait',
     icon: './src/assets/images/appicon.png',
     userInterfaceStyle: 'automatic',
