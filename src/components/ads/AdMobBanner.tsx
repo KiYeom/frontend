4a914931c8dfb670ea6742c2c4cf6b850b371628
ceunnseo <eunseo4068@gmail.com>
2025-06-20 13:10:41 +0900

    배너 ID 적용

--- src/components/ads/AdMobBanner.tsx
+++ src/components/ads/AdMobBanner.tsx
@@ -2,6 +2,16 @@
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 import { View, Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
+
+const appVariant = Constants.expoConfig?.extra?.appVariant;
+const isProductionOrStaging = appVariant === 'production' || appVariant === 'staging';
+const adUnitId = isProductionOrStaging
+  ? Platform.OS === 'android'
+    ? process.env.EXPO_PUBLIC_CHATTING_BANNER_AD_UNIT_ID_ANDROID
+    : process.env.EXPO_PUBLIC_CHATTING_BANNER_AD_UNIT_ID_IOS
+  : TestIds.REWARDED;
+
 const AdMobBanner = () => {
   const insets = useSafeAreaInsets();
   return (
@@ -14,7 +24,7 @@ const AdMobBanner = () => {
         zIndex: 101,
         //paddingTop: insets.top, // Adjust padding to avoid overlap with status bar
       }}>
-      <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.BANNER} />
+      <BannerAd unitId={adUnitId} size={BannerAdSize.BANNER} />
     </View>
   );
 };
