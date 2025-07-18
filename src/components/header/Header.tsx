6d2c821ad5508967efda7724b40e4e6497ce4442
ceunnseo <eunseo4068@gmail.com>
2025-07-18 17:17:57 +0900

    Fix filename casing issues

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,105 @@
+import * as amplitude from '@amplitude/analytics-react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import palette from '../../assets/styles/theme';
+import { rsHeight, rsWidth } from '../../utils/responsive-size';
+import Icon, { TIconName } from '../icons/icons';
+import {
+  HeaderCenter,
+  HeaderContainer,
+  HeaderEvent,
+  HeaderLeft,
+  HeaderRight,
+  HeaderTitle,
+  OptionText,
+} from './Header.styles';
+
+export type HeaderProps = {
+  title?: string;
+
+  isLeft?: boolean;
+  leftIcon?: TIconName;
+  leftText?: string;
+  leftFunction?: () => void;
+
+  isRight?: boolean;
+  rightIcon?: TIconName;
+  rightText?: string;
+  rightFunction?: () => void;
+
+  isEvent?: boolean;
+  eventIcon?: TIconName;
+  eventText?: string;
+  eventFunction?: () => void;
+
+  bgcolor?: string;
+  isDark?: boolean;
+};
+const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
+
+  const {
+    title = undefined,
+    isLeft = true,
+    leftIcon = 'arrow-left',
+    leftText = undefined,
+    leftFunction = () => {
+      navigation.goBack();
+      amplitude.track('뒤로가기 버튼 클릭');
+    },
+
+    isRight = false,
+    rightIcon,
+    rightText = undefined,
+    rightFunction = () => {},
+
+    isEvent = false,
+    eventIcon = 'event-icon',
+    eventText = undefined,
+    eventFunction = () => {},
+    bgcolor = 'white',
+    isDark = false,
+  } = props;
+  return (
+    <HeaderContainer
+      isTitle={title !== undefined}
+      insets={insets}
+      bgcolor={bgcolor}
+      isDark={isDark}>
+      <HeaderCenter>
+        <HeaderTitle ellipsizeMode="tail" numberOfLines={1} isDark={isDark}>
+          {title}
+        </HeaderTitle>
+      </HeaderCenter>
+
+      {isLeft && (
+        <HeaderLeft onPress={leftFunction} activeOpacity={1} isTitle={title !== undefined}>
+          <Icon
+            name={leftIcon}
+            width={rsWidth * 9 + 'px'}
+            color={!isDark ? palette.neutral[900] : palette.neutral[50]}
+          />
+          {leftText !== undefined && <OptionText>{leftText}</OptionText>}
+        </HeaderLeft>
+      )}
+
+      {isRight && (
+        <HeaderRight onPress={rightFunction} activeOpacity={1} isTitle={title !== undefined}>
+          {rightText !== undefined && <OptionText>{rightText}</OptionText>}
+          {rightIcon !== undefined && (
+            <Icon name={rightIcon} height={rsHeight * 30 + 'px'} color={palette.neutral[900]} />
+          )}
+        </HeaderRight>
+      )}
+      {isEvent && (
+        <HeaderEvent onPress={eventFunction} activeOpacity={1} isTitle={title !== undefined}>
+          <Icon name={eventIcon} width={rsWidth * 30 + 'px'} />
+          {eventText !== undefined && <OptionText>{eventText}</OptionText>}
+        </HeaderEvent>
+      )}
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/switch/Switch.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Animated, Easing } from 'react-native';
+import palette from '../../assets/styles/theme';
+import { animatedViewStyle, SwitchContainer } from './Switch.styles';
+
+export type SwitchProps = {
+  isEnabled: boolean;
+  disabled?: boolean;
+  onPress?: () => void;
+};
+
+const SwitchComponent = (props: SwitchProps) => {
+  const { isEnabled, disabled = false, onPress = () => {} } = props;
+  const [animatedValue] = useState(new Animated.Value(isEnabled ? 1 : 0));
+  const color = disabled
+    ? palette.neutral[100]
+    : isEnabled
+      ? palette.primary[500]
+      : palette.neutral[200];
+
+  const translateX = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 17],
+  });
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: isEnabled ? 1 : 0,
+      duration: 200,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    }).start();
+  }, [isEnabled, animatedValue]);
+
+  return (
+    <SwitchContainer onPress={onPress} color={color} disabled={disabled} activeOpacity={1}>
+      <Animated.View
+        style={{
+          ...animatedViewStyle,
+          transform: [{ translateX }],
+        }}
+      />
+    </SwitchContainer>
+  );
+};
+
+export default SwitchComponent;
